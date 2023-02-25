02ed91b1a63ee885c99a836fb0684051981d8881
inder vuejs <indra.tiger21@gmail.com>
2023-02-26 01:56:28 +0530

    database connected removed

--- application.py
+++ application.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
+import bson.json_util as json_util
+import databaseconnection
 import testingImport
+
 application = Flask(__name__)
+
+# client = databaseconnection.connectDb()
+
 
 @application.route("/")
 def hello_world():
-    return "Hello there"
-    
+    userdb = client["pinak"]
+    adminCol = userdb["users"]
+    allenteries = adminCol.find()
+    return json_util.dumps(allenteries)
+    # return "Hello there! I have some changes"
+
 @application.route("/hello-world2")
 def hello_world2():
-    return "Hello2 there!"
+    return testingImport.thisistesting()
