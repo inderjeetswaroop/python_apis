4219e64bf50e36c81ff86421d9702e98d72cb4df
inder vuejs <indra.tiger21@gmail.com>
2023-02-26 01:23:48 +0530

    database connected removed

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import testingImport
 application = Flask(__name__)
 
 @application.route("/")
 def hello_world():
     return "Hello there"
     
+@application.route("/hello-world2")
+def hello_world2():
+    return testingImport.thisistesting()    
