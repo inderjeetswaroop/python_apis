741b7cccee8d924180edfed6c1c12a52f4d5cbcd
inder vuejs <indra.tiger21@gmail.com>
2023-02-26 01:00:03 +0530

    database connected removed

--- application.py
+++ application.py
@@ -2,20 +2,21 @@ from flask import Flask, jsonify, render_template, request
 import bson.json_util as json_util
 import pymongo
 import databaseconnection
+import testingImport
 
 application = Flask(__name__)
 
-client = databaseconnection.connectDb()
+# client = databaseconnection.connectDb()
 
 
 @application.route("/")
 def hello_world():
-    userdb = client["pinak"]
+    """ userdb = client["pinak"]
     adminCol = userdb["users"]
     allenteries = adminCol.find()
-    return json_util.dumps(allenteries)
+    return json_util.dumps(allenteries) """
     return "Hello there! I have some changes"
 
 @application.route("/hello-world2")
 def hello_world2():
-    return "Hello world 2 there! I have been created by now"
+    return testingImport.thisistesting()
