89c1fd24358ac23937dfece84dc3fc94fe7a99de
inder vuejs <indra.tiger21@gmail.com>
2023-02-26 02:53:17 +0530

    database connection removed

--- application.py
+++ application.py
@@ -1,21 +1,21 @@
 from flask import Flask, jsonify, render_template, request
 import bson.json_util as json_util
-import databaseconnection
+# import databaseconnection
 import testingImport
 import pymongo
 
 application = Flask(__name__)
 
-client = databaseconnection.connectDb()
+# client = pymongo.MongoClient("mongodb+srv://mongoadmin:tHca1yxBGYrplCtj@pai-mongo-cluster.rwmz5rj.mongodb.net/?retryWrites=true&w=majority")
 
 
 @application.route("/")
 def hello_world():
-    userdb = client["pinak"]
+    """ userdb = client["pinak"]
     adminCol = userdb["users"]
     allenteries = adminCol.find()
-    return json_util.dumps(allenteries)
-    # return "Hello there! I have some changes"
+    return json_util.dumps(allenteries) """
+    return "Hello there! I have some changes"
 
 @application.route("/hello-world2")
 def hello_world2():
